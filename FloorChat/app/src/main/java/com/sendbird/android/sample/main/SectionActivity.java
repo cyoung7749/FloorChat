40edbaacf166a85878a74ad529fea5de3c4cfb46
Matthew Jackson <mrj81@scarletmail.rutgers.edu>
2019-12-09 20:05:37 -0500

    Added FAB to section screen

--- FloorChat/app/src/main/java/com/sendbird/android/sample/main/SectionActivity.java
+++ FloorChat/app/src/main/java/com/sendbird/android/sample/main/SectionActivity.java
@@ -1,9 +1,12 @@
 package com.sendbird.android.sample.main;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.sendbird.android.sample.R;
 
@@ -13,6 +16,7 @@ public class SectionActivity extends AppCompatActivity {
 
     private RecyclerView sectionRecyclerView;
     SectionAdapter adapter;
+    FloatingActionButton openChatFAB;
 
     ArrayList<Section> sections;
 
@@ -21,6 +25,15 @@ public class SectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section);
 
+        openChatFAB = findViewById(R.id.openChatFAB);
+
+        openChatFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openChatActivity();
+            }
+        });
+
         sectionRecyclerView = findViewById(R.id.sectionRecycler);
         sectionRecyclerView.setHasFixedSize(true);
         sectionRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
@@ -28,5 +41,10 @@ public class SectionActivity extends AppCompatActivity {
         sections = (ArrayList<Section>) getIntent().getSerializableExtra("sections");
         adapter = new SectionAdapter(sections);
         sectionRecyclerView.setAdapter(adapter);
+    }
+
+    public void openChatActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
